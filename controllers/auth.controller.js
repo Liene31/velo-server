a27a9e4f8120be2f9da34edb24f17fda155a1713
Liene <lienebare@gmail.com>
2026-04-21 15:11:02 +0200

    add register user service

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,6 +5,16 @@ export const authController = {
     try {
       const userToAdd = req.body;
       const emailExist = await authService.emailExist(userToAdd.email);
+
+      if (emailExist) {
+        return res.status(409).json({ message: `Email already exists` });
+      }
+
+      const createdUser = await authService.create(userToAdd);
+      //res.location(`/api/user/${userCreated._id}`);
+      res.status(201).json({
+        id: createdUser._id,
+      });
     } catch (err) {
       console.log(err);
       res.status(500).json({ statusCode: 500, message: "DB error" });
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,6 +1,26 @@
+import { User } from "../models/user.model.js";
+
 export const authService = {
   emailExist: async (email) => {
     try {
+      const userFound = await User.findOne({ email });
+      if (userFound) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+  },
+
+  create: async (user) => {
+    try {
+      //hashed password to add
+      const userToCreate = User(user);
+      await userToCreate.save();
+      return userToCreate;
     } catch (err) {
       console.log(err);
       throw new Error(err);
